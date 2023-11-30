debfad615665d2253142bd9ba0602b0d288216b1
kkkkYoungJae <dudwodla123@gmail.com>
2023-11-30 09:18:42 +0900

    인트로 동영상 구현

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,10 +1,17 @@
 import classNames from 'classnames/bind'
+import Section from '../shared/Section'
 import styles from './Video.module.scss'
 
 const cx = classNames.bind(styles)
 
 const Video = () => {
-  return <div>Video</div>
+  return (
+    <Section className={cx('container')}>
+      <video autoPlay muted loop poster="/assets/poster.jpg">
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
