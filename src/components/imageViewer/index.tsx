a4a660aa35d8e2f2614bcec0ffa7d33a547c0f1a
kkkkYoungJae <dudwodla123@gmail.com>
2023-11-30 09:57:51 +0900

    이미지 뷰어

---
+++ src/components/imageViewer/index.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames/bind'
+import 'swiper/css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import styles from './ImageViewer.module.scss'
+import './swiper.css'
+
+const cx = classNames.bind(styles)
+
+const ImageViewer = ({
+  images,
+  open = false,
+  selectedIdx,
+  onClose,
+}: {
+  images: string[]
+  open?: boolean
+  selectedIdx: number
+  onClose: () => void
+}) => {
+  if (open == false) {
+    return null
+  }
+
+  return (
+    <div className={cx('dimmed')}>
+      <CloseButton className={cx('icon-close')} onClose={onClose} />
+      <Swiper spaceBetween={20} slidesPerView={1} loop initialSlide={selectedIdx}>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt="이미지 뷰어" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
+
+const CloseButton = ({ onClose, className }: { className: string; onClose: () => void }) => {
+  return (
+    <svg className={className} id="Layer_1" viewBox="0 0 200 200" onClick={onClose}>
+      <path d="M100,15a85,85,0,1,0,85,85A84.93,84.93,0,0,0,100,15Zm0,150a65,65,0,1,1,65-65A64.87,64.87,0,0,1,100,165Z" />
+      <path d="M128.5,74a9.67,9.67,0,0,0-14,0L100,88.5l-14-14a9.9,9.9,0,0,0-14,14l14,14-14,14a9.9,9.9,0,0,0,14,14l14-14,14,14a9.9,9.9,0,0,0,14-14l-14-14,14-14A10.77,10.77,0,0,0,128.5,74Z" />
+    </svg>
+  )
+}
+export default ImageViewer
--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,20 +1,43 @@
 import classNames from 'classnames/bind'
+import { useState } from 'react'
+import ImageViewer from '../imageViewer'
 import Section from '../shared/Section'
 import styles from './ImageGallery.module.scss'
 
 const cx = classNames.bind(styles)
 
 const ImageGallery = ({ images }: { images: string[] }) => {
+  const [selectedIdx, setSelectedIdx] = useState(-1)
+
+  const open = selectedIdx > -1
+
+  const handleSelectedImage = (idx: number) => {
+    setSelectedIdx(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIdx(-1)
+  }
+
   return (
-    <Section title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, index) => (
-          <li key={index} className={cx('wrap-image')}>
-            <img src={src} alt="사진첩 이미지" />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, index) => (
+            <li
+              key={index}
+              className={cx('wrap-image')}
+              onClick={() => {
+                handleSelectedImage(index)
+              }}
+            >
+              <img src={src} alt="사진첩 이미지" />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageViewer images={images} open={open} selectedIdx={selectedIdx} onClose={handleClose} />
+    </>
   )
 }
 
