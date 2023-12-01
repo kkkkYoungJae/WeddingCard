ab23fd39b684f791cb8ec7d6216087d80e594012
kkkkYoungJae <dudwodla123@gmail.com>
2023-12-01 12:35:30 +0900

    커스텀훅 분리, react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -8,52 +8,21 @@ import Invitation from '@components/sections/Invitation'
 import Map from '@components/sections/Map'
 import Share from '@components/sections/Share'
 import Video from '@components/sections/Video'
-import { Wedding } from '@models/wedding'
 import FullScreenMessage from '@shared/FullScreenMessage'
 import classNames from 'classnames/bind'
-import { useEffect, useState } from 'react'
 import styles from './App.module.scss'
+import useWedding from './hooks/useWedding'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [count, setCount] = useState(0)
-
-  // 1. 데이터 호출
-  useEffect(() => {
-    setLoading(true)
-
-    fetch('http://localhost:8888/wedding')
-      .then((response) => {
-        if (response.ok === false) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
-        }
-        return response.json()
-      })
-      .then((data) => {
-        setWedding(data)
-      })
-      .catch((e) => {
-        console.log(e)
-        setError(true)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [])
-
-  if (loading) {
-    return <FullScreenMessage type="loading" />
-  }
+  const { wedding, isLoading, error } = useWedding()
 
   if (error) {
     return <FullScreenMessage type="error" />
   }
 
-  if (wedding === null) return null
+  if (!wedding) return null
 
   const {
     date,
@@ -66,9 +35,6 @@ function App() {
 
   return (
     <div className={cx('container')}>
-      <button style={{ position: 'fixed', top: 0 }} onClick={() => setCount((prev) => prev + 1)}>
-        +{count}
-      </button>
       <Heading date={date} />
       <Video />
       <Intro
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import { ModalContext } from '@contexts/ModalContext'
+import '@scss/global.scss'
+import FullScreenMessage from '@shared/FullScreenMessage'
+import { Suspense } from 'react'
 import 'react-day-picker/dist/style.css'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
-import './scss/global.scss'
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <ModalContext>
-    <App />
-  </ModalContext>,
+  <QueryClientProvider client={queryClient}>
+    <ModalContext>
+      <Suspense fallback={<FullScreenMessage type="loading" />}>
+        <App />
+      </Suspense>
+    </ModalContext>
+  </QueryClientProvider>,
 )
