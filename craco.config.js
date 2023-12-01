f1080fe4fae7ed7fbb42213ce9acde8abd16dace
kkkkYoungJae <dudwodla123@gmail.com>
2023-12-01 11:55:54 +0900

    폰트 최적화

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,19 @@
 const CracoAlias = require('craco-alias')
+const FontPreloadPlugin = require('webpack-font-preload-plugin')
 
-module.exports ={
-    plugins:[
-        {
-            plugin:CracoAlias,
-            options:{
-                source:'tsconfig',
-                tsConfigPath:'tsconfig.paths.json'
-            }
-        }
-    ]
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+  webpack: {
+    plugins: {
+      add: [new FontPreloadPlugin()],
+    },
+  },
 }
