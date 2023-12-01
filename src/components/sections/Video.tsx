c4bb3a66c46f56654686f50a8932678733837500
kkkkYoungJae <dudwodla123@gmail.com>
2023-12-01 10:45:24 +0900

    동영상 최적화

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -8,6 +8,7 @@ const Video = () => {
   return (
     <Section className={cx('container')}>
       <video autoPlay muted loop poster="/assets/poster.jpg">
+        <source src="/assets/main.webm" type="video/webm" />
         <source src="/assets/main.mp4" type="video/mp4" />
       </video>
     </Section>
